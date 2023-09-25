0ce78f4a6e0d9d086abce207c8bba37a109bbe4c
Jayzilva <10748102@students.plymouth.ac.uk>
2023-09-25 15:11:41 +0530

    update

---
+++ front-end/components/UserLayout.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { ConnectWallet } from "@thirdweb-dev/react";
+import Link from 'next/link';
+
+export default function UserLayout() {
+
+
+  return (
+<nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-cyan-500 to-blue-500 p-10">
+<div className="flex items-center flex-shrink-0 text-white mr-7">
+
+  <span className="font-semibold text-1xl tracking-tight"><Link  href="/" className="font-semibold text-3xl tracking-tight">Shoozy </Link>Manufacture-Portal</span>
+</div>
+<div className="block lg:hidden">
+  <button className="flex items-center px-3 py-2 border rounded text-black-200 border-teal-400 hover:text-white hover:border-white">
+    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+  </button>
+</div>
+<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto ">
+  <div className="text-sm lg:flex-grow ">
+    <Link  href="/" className="text-lg font-medium block mt-5 lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
+      Movies
+    </Link >
+    <Link  href="/AdminViewMovies" className=" text-lg font-medium block mt-5 lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
+      But Tickets
+    </Link >
+  </div>
+  <ConnectWallet />
+  <div>
+   
+  </div>
+</div>
+</nav>
+  )
+}
---
+++ front-end/pages/AdminAddMovies.js
@@ -0,0 +1,74 @@
+import AdminLayout from "../components/AdminLayout";
+import {
+  useAddress,
+  useContract,
+  useContractRead,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import React, { useEffect, useState } from "react";
+
+
+function AdminAddMovies() {
+  const address = useAddress();
+  const contractAdd = "0x8105589c0658cD725Dc8067A6001059D94f31deA";
+  const { contract } = useContract(contractAdd);
+  const { mutateAsync: addRetailer } = useContractWrite(
+    contract,
+    "addRetailer"
+  );
+  const [id, setId] = useState(0);
+  const [name, setName] = useState("");
+
+
+  const handleComplaint = async () => {
+    try {
+      const data = await addRetailer([id, name, addr]);
+
+      alert(`Retailer Added`);
+      console.info("contract call successs", data);
+      setId("");
+      setName("");
+      setAddr("");
+    } catch (err) {
+      const errorMessage = err.message.match(/Reason: (.*)/);
+        alert(errorMessage);
+        
+
+        console.error("contract call failure", err);
+
+    }
+  };
+
+  return (
+    <div>
+       
+       <>
+          <AdminLayout />
+      <div class="w-96 m-10">
+      <h1 class="text-4xl font-bold">Add Retailers</h1>
+      <br/>
+      <div class="mb-6">
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Movie ID</label>
+              <input type="text" placeholder="ID" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " onChange={(e) => {setId(e.target.value);}} required/>
+      </div>
+      <div class="mb-6">
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Movie Name</label>
+              <input type="text" placeholder="Name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "  onChange={(e) => {setName(e.target.value);}} required/>
+      </div>
+
+        <button class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+          onClick={() => {
+            handleComplaint();
+          }}
+        >
+          Add Movie
+        </button>
+      </div>
+          </>
+        
+        
+    </div>
+  )
+}
+
+export default AdminAddMovies
