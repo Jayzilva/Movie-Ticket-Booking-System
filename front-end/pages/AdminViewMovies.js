1a7d5bec25cb0c1cf03065720b1511b4a65ffca5
Jayath de Silva <116673159+Jayzilva@users.noreply.github.com>
2023-09-27 19:57:31 +0530

    Update AdminViewMovies.js

--- front-end/pages/AdminViewMovies.js
+++ front-end/pages/AdminViewMovies.js
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react';
 
 function AdminViewMovies() {
   const address = useAddress();
-  const contractAdd ="0xA7Ed3dA7A0132111Ad1AB8aB337606C78E52Ca0b";
+  const contractAdd ="0xD06B8C3c14cc55629Bd6a1e71ddBF0323C88AE36";
   const { contract } = useContract(contractAdd);
   const { data: viewMovies } = useContractRead(contract, "viewMovies");
   const [movies, setMovies] = useState([]);
